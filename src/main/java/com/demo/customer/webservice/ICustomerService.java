6511805146721cb91722b9e17f644a9a2e56be67
wkm23@126.com <wkm23@126.com>
2016-07-11 09:47:57 +1200

    DemoCustomer init

--- src/main/java/com/demo/customer/webservice/ICustomerService.java
+++ src/main/java/com/demo/customer/webservice/ICustomerService.java
@@ -9,30 +9,24 @@ import javax.ws.rs.core.Response;
  * The CustomerServiceImpl implement simple CRUD of a customer
  * Created by Kyle
  */
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public interface ICustomerService{
 
     @GET
     @Path("/customers/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Response getCustomer(@PathParam("id") Long id);
 
     @PUT
     @Path("/customers/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Response updateCustomer(@PathParam("id") Long id, String info);
 
     @POST
     @Path("/customers")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Response addCustomer(String info);
 
     @DELETE
     @Path("/customers/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Response deleteCustomer(@PathParam("id") Long id);
 
 }
